c0f72624e24889bf0082c82711ed2975cd598b4e
Alex <alexkorman@gmail.com>
2025-11-07 14:02:15 -0500

    fix: add back validation to send-invites form

--- app/(protected)/onboarding/send-invites.tsx
+++ app/(protected)/onboarding/send-invites.tsx
@@ -62,7 +62,7 @@ export default function SendInvitesPage() {
   const forms = [form1, form2];
 
   // Check if all 2 forms are valid //TODO: Remove this
-  const allFormsValid = true; //form1.formState.isValid && form2.formState.isValid;
+  const allFormsValid = form1.formState.isValid && form2.formState.isValid;
 
   const pickContact = async (formIndex: number) => {
     const phoneNumber = await pickContactFromDevice();
